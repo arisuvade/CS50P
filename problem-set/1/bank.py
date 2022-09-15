537abe800d247d6da51066ca8b526c8674704fb3
Aries Dave Bautista <110705264+arisuvade@users.noreply.github.com>
2022-09-15 15:13:52 +0800

    Update bank.py

--- problem-set/1/bank.py
+++ problem-set/1/bank.py
@@ -1,7 +1,10 @@
 forbidden_letter = "h"
+forbidden_greeting = "hello"
 greeting = input("Greeting: ")
 
 if greeting == "hello":
+    print("$0")
+elif greeting >= forbidden_greeting:
     print("$0")
 elif greeting >= forbidden_letter:
     print("$20")
